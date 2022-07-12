045d3e64bdde520dc1d25758a28f8e95fcfa1f70
Talakoobi <s289641@studenti.polito.it>
2022-07-12 14:15:09 +0200

    added colab codes

---
+++ colab code/ACTOR_CRITIC_A_F/train.py
@@ -0,0 +1,101 @@
+"""Train an RL agent on the OpenAI Gym Hopper environment
+
+TODO: implement 2.2.a and 2.2.b
+"""
+
+from operator import contains
+from unittest.mock import Base
+import torch
+import gym
+import argparse
+import os
+from env.custom_hopper import *
+from agent import Agent, Policy
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--n-episodes", default=100000, type=int, help="Number of training episodes"
+    )
+    parser.add_argument(
+        "--print-every", default=1000, type=int, help="Print info every <> episodes"
+    )
+    parser.add_argument(
+        "--device", default="cpu", type=str, help="network device [cpu, cuda]"
+    )
+    parser.add_argument(
+        "--lr", default=1e-3, type=float, help="learning rate"
+    )
+
+    return parser.parse_args()
+
+
+args = parse_args()
+
+
+def main():
+
+    # torch.cuda.set_per_process_memory_fraction(0.7, 0)
+
+    env = gym.make("CustomHopper-source-v0")
+    # env = gym.make('CustomHopper-target-v0')
+
+    print("Action space:", env.action_space)
+    print("State space:", env.observation_space)
+    print("Dynamics parameters:", env.get_parameters())
+
+    """
+		Training
+	"""
+    observation_space_dim = env.observation_space.shape[-1]
+    action_space_dim = env.action_space.shape[-1]
+    reward_log = []
+    best_reward = -1000
+    policy = Policy(observation_space_dim, action_space_dim)
+    lr = args.lr
+    agent = Agent(policy, device=args.device, lr = lr)
+
+    for episode in range(args.n_episodes):
+        done = False
+        train_reward = 0
+        state = env.reset()  # Reset the environment and observe the initial state
+
+        while not done:  # Loop until the episode is over
+
+            action, action_probabilities, value = agent.get_action(state)
+            previous_state = state
+
+            state, reward, done, info = env.step(action.detach().cpu().numpy())
+
+            agent.store_outcome(
+                previous_state, state, action_probabilities, reward, done, value
+            )
+
+            train_reward += reward
+
+        reward_log.append(train_reward)
+        if(train_reward > best_reward):
+            best_reward = train_reward
+            checkpoint = {
+                'model': agent.policy.state_dict(),
+                'n_episode' : episode + 1
+            }
+            if(not os.path.exists('./models')):
+                os.makedirs('./models')
+            torch.save(checkpoint, f"./models/best_lr_{lr}_nEpisodes_{args.n_episodes}.pt")
+        if (episode + 1) % args.print_every == 0:
+            print("Training episode:", episode)
+            print("Episode return:", train_reward)
+
+        agent.update_policy()
+        agent.reset_outcomes()
+    if(not os.path.exists('./train_history')):
+        os.makedirs('./train_history')
+    with open(f"./train_history/lr_{lr}_nEpisodes_{args.n_episodes}.txt", "w+") as f:
+        f.write(str(reward_log))
+
+    torch.save(agent.policy.state_dict(), "./models/last_model.mdl")
+
+if __name__ == "__main__":
+    main()
---
+++ colab code/ppo/train.py
@@ -0,0 +1,152 @@
+from email import policy
+from operator import contains
+from unittest.mock import Base
+import matplotlib.pyplot as plt
+
+import torch
+import gym
+import argparse
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.results_plotter import load_results, ts2xy, plot_results
+from stable_baselines3.common.monitor import Monitor
+
+
+
+import os
+from env.custom_hopper import *
+# from agent import Agent, Policy
+
+
+class SaveOnBestTrainingRewardCallback(BaseCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq:
+    :param log_dir: Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: Verbosity level.
+    """
+    def __init__(self, check_freq: int, log_dir: str, verbose: int = 1):
+        super(SaveOnBestTrainingRewardCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self.check_freq == 0:
+
+          # Retrieve training reward
+          x, y = ts2xy(load_results(self.log_dir), 'timesteps')
+          if len(x) > 0:
+              # Mean training reward over the last 100 episodes
+              mean_reward = np.mean(y[-100:])
+              if self.verbose > 0:
+                print(f"Num timesteps: {self.num_timesteps}")
+                print(f"Best mean reward: {self.best_mean_reward:.2f} - Last mean reward per episode: {mean_reward:.2f}")
+
+              # New best model, you could save the agent here
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = mean_reward
+                  # Example for saving best model
+                  if self.verbose > 0:
+                    print(f"Saving new best model to {self.save_path}")
+                  self.model.save(self.save_path)
+
+        return True
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--n-timesteps", default=100000, type=int, help="Number of training episodes"
+    )
+    parser.add_argument(
+        "--print-every", default=1000, type=int, help="Print info every <> episodes"
+    )
+    parser.add_argument(
+        "--device", default="cpu", type=str, help="network device [cpu, cuda]"
+    )
+    parser.add_argument(
+        "--lr", default="1e-3", type=float, help="lrarning rate"
+    )
+
+    return parser.parse_args()
+
+
+args = parse_args()
+
+
+def main():
+    # Create log dir
+    log_dir = "/tmp/gym/"
+    os.makedirs(log_dir, exist_ok=True)
+    # torch.cuda.set_per_process_memory_fraction(0.7, 0)
+
+    env = gym.make("CustomHopper-target-v0")
+    env = Monitor(env, log_dir)
+    # env = gym.make('CustomHopper-target-v0')
+
+    print("Action space:", env.action_space)
+    print("State space:", env.observation_space)
+    print("Dynamics parameters:", env.get_parameters())
+
+    """
+		Training
+	"""
+
+    model = PPO('MlpPolicy', env,learning_rate = args.lr,device= args.device,tensorboard_log="./a2c_cartpole_tensorboard/")
+    # Create the callback: check every 1000 steps
+    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
+    model.learn(total_timesteps = args.n_timesteps, callback=callback)
+
+    model.save("model.mdl")
+
+    from stable_baselines3.common import results_plotter
+
+# Helper from the library
+    results_plotter.plot_results([log_dir], 1e5, results_plotter.X_TIMESTEPS, "PPO CustomHopper")
+
+
+    def moving_average(values, window):
+
+    # Smooth values by doing a moving average
+    # :param values: (numpy array)
+    # :param window: (int)
+    # :return: (numpy array)
+
+        weights = np.repeat(1.0, window) / window
+        return np.convolve(values, weights, 'valid')
+
+
+    def plot_results(log_folder, title='Learning Curve'):
+        """
+        plot the results
+
+        :param log_folder: (str) the save location of the results to plot
+        :param title: (str) the title of the task to plot
+        """
+        x, y = ts2xy(load_results(log_folder), 'timesteps')
+        y = moving_average(y, window=50)
+        # Truncate x
+        x = x[len(x) - len(y):]
+
+        fig = plt.figure(title)
+        plt.plot(x, y)
+        plt.xlabel('Number of Timesteps')
+        plt.ylabel('Rewards')
+        plt.title(title + " Smoothed")
+        # plt.show()
+        plt.savefig(f"./{args.lr}.png")
+
+    plot_results(log_dir)
+
+if __name__ == "__main__":
+    main()
